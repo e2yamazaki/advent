0389bebdcd38ae896f116ff04c9bee20315a61e4
yamazaki <yamazaki@e-2.co.jp>
2021-12-04 20:49:57 +0900

    本文の改行を反映

--- pages/post/_id.vue
+++ pages/post/_id.vue
@@ -4,7 +4,7 @@
   <div class="container px-5 py-24 mx-auto">
     <div class="xl:w-1/2 lg:w-3/4 w-full mx-auto text-center">
       <header class="pt-6 xl:pb-10"><div class="space-y-1 text-center"><dl class="space-y-10"><div><dt class="sr-only">Published on</dt><dd class="text-base leading-6 font-medium text-gray-500">{{new Date(created_at).toLocaleString('ja-JP')}}</dd></div></dl><div><h1 class="text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl md:text-5xl md:leading-[3.5rem]">{{title}}</h1></div></div></header>
-      <p class="leading-relaxed text-lg">{{body}}</p>
+      <p class="leading-relaxed text-lg" style="white-space: pre-wrap;">{{body}}</p>
       <span class="inline-block h-1 w-10 rounded bg-blue-500 mt-8 mb-6"></span>
       <div>
         <a class="inline-flex items-center">
